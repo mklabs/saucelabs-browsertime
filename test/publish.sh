8040851ddd283ad063a9386f634992f548a12abe
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-02 05:18:25 +0200

    scripts - force gh-pages checkout

--- test/publish.sh
+++ test/publish.sh
@@ -4,12 +4,17 @@ cd $TRAVIS_BUILD_DIR
 
 git remote set-branches --add origin $DEPLOY_BRANCH || exit 1
 echo "Remote set to $DEPLOY_BRANCH"
+
 git fetch -q || exit 1
+
 git branch $DEPLOY_BRANCH origin/$DEPLOY_BRANCH || exit 1
+git checkout $DEPLOY_BRANCH || exit 1
 echo "Branch set up to origin/$DEPLOY_BRANCH"
 echo "Cd to Build #$TRAVIS_BUILD_NUMBER directory"
+
 cd results/$TRAVIS_BUILD_NUMBER || exit 1
 ls -la || exit 1
+
 git add . -f || exit 1
 git status || exit 1
 git commit -m "Configure Travis to build and publish site" || exit 1
