1208a6d3248f3385bec1096bfd17a4385dbfefe0
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-02 10:18:44 +0200

    scripts - generate index in postbuild script

--- test/publish.sh
+++ test/publish.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR
+RESULT_DIR=results/$TRAVIS_BUILD_NUMBER
 
+git fetch -q || exit 1
 git remote set-branches --add origin $DEPLOY_BRANCH || exit 1
 echo "Remote set to $DEPLOY_BRANCH"
 
-git fetch -q || exit 1
+git checkout -b $DEPLOY_BRANCH origin/$DEPLOY_BRANCH || git checkout $DEPLOY_BRANCH || exit 1
+git status || exit 1
 
-echo "Branch set up to build-$TRAVIS_BUILD_NUMBER"
+echo "... Generate  $RESULT_DIR/index.html ..."
+node test/build-index.js $RESULT_DIR > $RESULT_DIR/index.html
+
+echo "... Generate  results/index.html ..."
+node test/build-index.js $RESULT_DIR index > results/index.html
+
+echo "... Generate index.html ..."
+node test/build-index.js $RESULT_DIR index readme.md > index.html
+
 git status | exit 1
+
 echo "Adding results"
-ls -la results || exit 1
 git add results -f || exit 1
 git add index.html || exit 1
 git status || exit 1
 
-git checkout -b $DEPLOY_BRANCH origin/$DEPLOY_BRANCH || git checkout $DEPLOY_BRANCH || exit 1
-
-git status
 
 git commit -F- <<EOF
 Travis publish $TRAVIS_COMMIT results ($TRAVIS_BUILD_NUMBER)
--- test/run.sh
+++ test/run.sh
@@ -29,12 +29,3 @@ for browser in $BROWSERS; do
   echo "... Generate  $RESULT_DIR/$browser.html ..."
   node bin/sauce-browsertime-html $RESULT_DIR/$browser/metrics.json -b "$logo" >  $RESULT_DIR/$browser.html
 done
-
-echo "... Generate  $RESULT_DIR/index.html ..."
-node test/build-index.js $RESULT_DIR > $RESULT_DIR/index.html
-
-echo "... Generate  results/index.html ..."
-node test/build-index.js $RESULT_DIR index > results/index.html
-
-echo "... Generate index.html ..."
-node test/build-index.js $RESULT_DIR index readme.md > index.html
