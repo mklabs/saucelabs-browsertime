668506a2c236efd6dcd204ef37ba73b14dad6e7d
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-02 08:47:35 +0200

    publish - merge -s recursive -X theirs

--- test/publish.sh
+++ test/publish.sh
@@ -24,7 +24,7 @@ EOF
 
 echo "Merging build-$TRAVIS_BUILD_NUMBER -> $DEPLOY_BRANCH"
 git checkout $DEPLOY_BRANCH || exit 1
-git merge build-$TRAVIS_BUILD_NUMBER -s theirs -m "Travis merge build $TRAVIS_BUILD_NUMBER" || exit 1
+git merge build-$TRAVIS_BUILD_NUMBER -s recursive -X theirs -m "Travis merge build $TRAVIS_BUILD_NUMBER" || exit 1
 
 echo "Cleanup build branch: b-$TRAVIS_BUILD_NUMBER"
 git branch -D build-$TRAVIS_BUILD_NUMBER
