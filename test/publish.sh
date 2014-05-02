af5b3d84192bccc0ab613c92a76850580caa8e9d
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-02 08:08:30 +0200

    publish - fix merge

--- test/publish.sh
+++ test/publish.sh
@@ -18,12 +18,12 @@ git status || exit 1
 
 git commit -m "Build #$TRAVIS_BUILD_NUMBER - Publishing $TRAVIS_COMMIT results\n\n$DEPLOY_URL/results/$TRAVIS_BUILD_NUMBER" || exit 1
 
-echo "Merging b-$TRAVIS_BUILD_NUMBER -> $DEPLOY_BRANCH"
+echo "Merging build-$TRAVIS_BUILD_NUMBER -> $DEPLOY_BRANCH"
 git checkout $DEPLOY_BRANCH || exit 1
-git merge b-$TRAVIS_BUILD_NUMBER || exit 1
+git merge build-$TRAVIS_BUILD_NUMBER || exit 1
 
 echo "Cleanup build branch: b-$TRAVIS_BUILD_NUMBER"
-git branch -D b-$TRAVIS_BUILD_NUMBER
+git branch -D build-$TRAVIS_BUILD_NUMBER
 
 echo "Pushing to repo.."
 git push origin $DEPLOY_BRANCH || exit 1
