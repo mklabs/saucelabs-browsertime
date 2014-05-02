bd64f55f7113c2e751c637e7695263acbd40a704
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-02 08:33:01 +0200

    publish - commit msg heredoc

--- test/publish.sh
+++ test/publish.sh
@@ -16,13 +16,15 @@ git add results -f || exit 1
 git add index.html || exit 1
 git status || exit 1
 
-msg="Travis publish $TRAVIS_COMMIT results ($TRAVIS_BUILD_NUMBER)\n\n$DEPLOY_URL/results/$TRAVIS_BUILD_NUMBER"
-echo $msg
-git commit -m "$msg" || exit 1
+git commit -F- <<EOF
+Travis publish $TRAVIS_COMMIT results ($TRAVIS_BUILD_NUMBER)
+
+$DEPLOY_URL/results/$TRAVIS_BUILD_NUMBER
+EOF
 
 echo "Merging build-$TRAVIS_BUILD_NUMBER -> $DEPLOY_BRANCH"
 git checkout $DEPLOY_BRANCH || exit 1
-git merge build-$TRAVIS_BUILD_NUMBER -s ours -m "$msg" || exit 1
+git merge build-$TRAVIS_BUILD_NUMBER -s ours -m "Travis merge build $TRAVIS_BUILD_NUMBER" || exit 1
 
 echo "Cleanup build branch: b-$TRAVIS_BUILD_NUMBER"
 git branch -D build-$TRAVIS_BUILD_NUMBER
