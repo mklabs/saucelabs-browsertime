cb17eaabe82c65f1728b6e8cf83e0e69b4a7084a
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-02 08:04:16 +0200

    scripts - publish, use build branch before merging

--- test/publish.sh
+++ test/publish.sh
@@ -7,16 +7,24 @@ echo "Remote set to $DEPLOY_BRANCH"
 
 git fetch -q || exit 1
 
-git branch $DEPLOY_BRANCH origin/$DEPLOY_BRANCH || exit 1
-git checkout $DEPLOY_BRANCH || exit 1
-echo "Branch set up to origin/$DEPLOY_BRANCH"
-echo "Cd to Build #$TRAVIS_BUILD_NUMBER directory"
+git checkout -b build-$TRAVIS_BUILD_NUMBER
 
+echo "Branch set up to build-$TRAVIS_BUILD_NUMBER"
+git status | exit 1
+echo "Adding results"
 git add results -f || exit 1
 git add index.html || exit 1
-
 git status || exit 1
+
 git commit -m "Build #$TRAVIS_BUILD_NUMBER - Publishing $TRAVIS_COMMIT results\n\n$DEPLOY_URL/results/$TRAVIS_BUILD_NUMBER" || exit 1
+
+echo "Merging b-$TRAVIS_BUILD_NUMBER -> $DEPLOY_BRANCH"
+git checkout $DEPLOY_BRANCH || exit 1
+git merge b-$TRAVIS_BUILD_NUMBER || exit 1
+
+echo "Cleanup build branch: b-$TRAVIS_BUILD_NUMBER"
+git branch -D b-$TRAVIS_BUILD_NUMBER
+
 echo "Pushing to repo.."
 git push origin $DEPLOY_BRANCH || exit 1
 echo "Published"
